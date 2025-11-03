5029a3600b2b9918fc11c6f9e500e32dc5c7399e
bethan <41525073+bethanw@users.noreply.github.com>
2025-11-03 16:34:03 +0000

    Update make_graph_out.R

--- make_graph_out.R
+++ make_graph_out.R
@@ -1,15 +1,17 @@
 make_graph_out <- function(out, bw, x_low = 0, x_high, include_legend = TRUE, add_smooth = FALSE, point_estimate = NA){
   out_df <- data.frame(out)
-  out_df$colours <- ifelse(out_df$out < 0.03, "Population risk", ifelse(out_df$out < 0.08, "Moderate risk", "High risk"))
+  out_df$colours <- ifelse(out_df$out < 0.03, "Near-population risk", ifelse(out_df$out < 0.08, "Moderate risk", "High risk"))
   
   plot <- ggplot(out_df, aes(x = out)) + geom_histogram(data = out_df, binwidth = bw, boundary = bw, aes(fill = colours)) + 
-    #labs(x = "10-year risk from 40", y = "Count", fill = "") + scale_fill_manual(values = c("Population risk" = "lightskyblue", "Moderate risk" = "orange", "High risk" = "red")) + 
-    labs(x = "10-year risk from 40", y = "Count", fill = "") + scale_fill_manual(values = c("Population risk" = "royalblue", "Moderate risk" = "orange", "High risk" = "red")) + 
+    #labs(x = "10-year risk from 40", y = "Count", fill = "") + scale_fill_manual(values = c("Near-population risk" = "lightskyblue", "Moderate risk" = "orange", "High risk" = "red")) + 
+    labs(x = "10-year risk from 40", y = "Count", fill = "") + scale_fill_manual(values = c("Near-population risk" = "royalblue", "Moderate risk" = "orange", "High risk" = "red")) + 
     theme(panel.background = element_rect(fill = "white"),
           panel.grid.major = element_line(linewidth = 0.1, linetype = 'solid', colour = "white"), 
           panel.grid.minor = element_line(linewidth = 0.1, linetype = 'solid', colour = "white"),
           axis.line = element_line(color = "black", size = 0.2)) + 
-    scale_y_continuous(expand = c(0, 0), limits = c(0, NA)) + scale_x_continuous(expand = c(0, 0), limits = c(x_low,x_high))
+    scale_y_continuous(expand = c(0, 0), limits = c(0, NA)) + 
+    # scale_x_continuous(expand = c(0, 0), limits = c(x_low,x_high))
+    scale_x_continuous(expand = expansion(mult = c(0, 0.1)), limits = c(x_low,x_high))
   
   if (include_legend == FALSE){
     plot <- plot + theme(legend.position="none")
