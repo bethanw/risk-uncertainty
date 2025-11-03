1324aca2de4fd6fcaf83b3f093e24b9d9108b421
bethan <41525073+bethanw@users.noreply.github.com>
2025-11-03 16:18:41 +0000

    Update save_dataset.R

--- save_dataset.R
+++ save_dataset.R
@@ -1,8 +1,8 @@
-save_dataset <- function(dataset, path = "~/OneDrive/DEV-Models-Batching-4 copy/data/", filename = "default"){
+save_dataset <- function(dataset, filepath = "default"){
   if (filename == "default"){
-    filename_location <- paste(path, deparse(substitute(dataset)), ".csv", sep = "")
+    filename_location <- paste(deparse(substitute(dataset)), ".csv", sep = "")
   } else {
-    filename_location <- paste(path, filename, ".csv", sep = "")
+    filename_location <- filepath
   }
   write.csv(dataset, filename_location, quote = FALSE, row.names = FALSE)
 }
